c038720149c6b42359b38d73b608eedb58763e37
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-07-07 09:56:51 +0700

    add assets

---
+++ src/assets/icons/SearchIcon.vue
@@ -0,0 +1,23 @@
+<template lang="">
+    <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M9.16728 15.8333C12.8495 15.8333 15.8346 12.8486 15.8346 9.16667C15.8346 5.48477 12.8495 2.5 9.16728 2.5C5.48504 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48504 15.8333 9.16728 15.8333Z"
+            stroke="#9097AC"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+        <path
+            d="M17.5016 17.5L13.8763 13.875"
+            stroke="#9097AC"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        />
+    </svg>
+</template>
+<script>
+    export default {
+        name: 'SearchIcon',
+    };
+</script>
