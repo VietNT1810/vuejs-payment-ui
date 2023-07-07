975416189296833d08760f40f95772ae7c260333
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-07-07 09:56:23 +0700

    config project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Payment />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import Payment from '@/pages/Payment.vue';
+    export default {
+        setup() {},
+        components: {
+            Payment,
+        },
+    };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/css/main.scss';
+import store from './store';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(store);
+app.use(ElementPlus);
+app.mount('#app');
+
+// createApp(App).use(store).use(ElementPlus).mount('#app');
