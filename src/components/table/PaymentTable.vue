a380cc6394a23c4d435c5773d86701edf8fc6b38
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-07-07 17:03:11 +0700

    payment table

--- src/components/table/PaymentTable.vue
+++ src/components/table/PaymentTable.vue
@@ -1,6 +1,6 @@
 <template lang="">
     <el-table class="table" :data="tableData" style="width: 100%">
-        <el-table-column label="Customer" min-width="220" fixed>
+        <el-table-column label="Customer" width="250" fixed>
             <template #default="scope">
                 <div class="customer">
                     <img :src="scope.row.avatar" class="avatar" alt="Customer avatar" />
@@ -11,29 +11,29 @@
                 </div>
             </template>
         </el-table-column>
-        <el-table-column label="Product" width="200">
+        <el-table-column label="Product" width="">
             <template #default="scope">
-                <p class="product">{{ scope.row.product }}</p>
+                <p class="product">{{ getProductName(scope.row.product) }}</p>
             </template>
         </el-table-column>
-        <el-table-column label="Date" width="200" sortable>
+        <el-table-column prop="date.started" label="Date" width="200" sortable>
             <template #default="scope">
                 <div class="date">
                     <div class="started">
                         <span class="title">Started: </span>
-                        <span class="text">{{ scope.row.date.started }}</span>
+                        <span class="text">{{ formatDate(scope.row.date.started) }}</span>
                     </div>
                     <div v-if="scope.row.date.ended" class="ended">
                         <span class="title">Ended: </span>
-                        <span class="text">{{ scope.row.date.ended }}</span>
+                        <span class="text">{{ formatDate(scope.row.date.ended) }}</span>
                     </div>
                 </div>
             </template>
         </el-table-column>
-        <el-table-column label="Amount" sortable width="100">
+        <el-table-column prop="amount" label="Amount" sortable width="100">
             <template #default="scope">
                 <div class="amount">
-                    <span class="value">${{ scope.row.amount }}</span>
+                    <span class="value">${{ scope.row.amount.toFixed(2) }}</span>
                     <span class="status">
                         <div v-if="scope.row.status == 'ACTIVE'" class="inner">
                             <ActiveIcon />
@@ -48,31 +48,34 @@
             </template>
         </el-table-column>
         <el-table-column label="Actions" width="50" fixed="right">
-            <el-dropdown
-                class="actions"
-                trigger="click"
-                :popper-options="{ modifiers: [{ name: 'offset', options: { offset: [-34, 10] } }] }"
-            >
-                <button class="actions-btn">
-                    <ActionsIcon />
-                </button>
-                <template #dropdown>
-                    <el-dropdown-menu>
-                        <el-dropdown-item>
-                            <div class="action-item">
-                                <HumanIcon />
-                                <span class="edit">Edit</span>
-                            </div>
-                        </el-dropdown-item>
-                        <el-dropdown-item
-                            ><div class="action-item">
-                                <DeleteIcon />
-                                <span class="delete">Delete</span>
-                            </div>
-                        </el-dropdown-item>
-                    </el-dropdown-menu>
-                </template>
-            </el-dropdown>
+            <template #default="scope">
+                <el-dropdown
+                    class="actions"
+                    trigger="click"
+                    @command="handleCommand"
+                    :popper-options="{ modifiers: [{ name: 'offset', options: { offset: [-34, 10] } }] }"
+                >
+                    <button class="actions-btn">
+                        <ActionsIcon />
+                    </button>
+                    <template #dropdown>
+                        <el-dropdown-menu>
+                            <el-dropdown-item :command="{ type: 'edit', id: scope.row.id }">
+                                <div class="action-item">
+                                    <HumanIcon />
+                                    <span class="edit">Edit</span>
+                                </div>
+                            </el-dropdown-item>
+                            <el-dropdown-item :command="{ type: 'delete', id: scope.row.id }">
+                                <div class="action-item">
+                                    <DeleteIcon />
+                                    <span class="delete">Delete</span>
+                                </div>
+                            </el-dropdown-item>
+                        </el-dropdown-menu>
+                    </template>
+                </el-dropdown>
+            </template>
         </el-table-column>
     </el-table>
 </template>
@@ -82,40 +85,20 @@
     import ActionsIcon from '@/assets/icons/ActionsIcon.vue';
     import HumanIcon from '@/assets/icons/HumanIcon.vue';
     import DeleteIcon from '@/assets/icons/DeleteIcon.vue';
+    import { formatDate, getProductName } from '@/helpers/string';
 
     export default {
-        setup() {
-            const tableData = [
-                {
-                    id: 1,
-                    avatar: 'https://i.pravatar.cc/100?u=fake1@pravatar.com',
-                    name: 'Arlene McCoy',
-                    email: 'sara.cruz@example.com',
-                    product: 'HTML / JS Course Bundle',
-                    date: {
-                        started: 'Sep 15, 2020',
-                        ended: 'Dec 30, 2021',
-                    },
-                    amount: '200.00',
-                    status: 'ACTIVE',
-                },
-                {
-                    id: 2,
-                    avatar: 'https://i.pravatar.cc/100?u=fake2@pravatar.com',
-                    name: 'Arlene McCree',
-                    email: 'sara.cruz@example.com',
-                    product: 'HTML / JS Course Bundle',
-                    date: {
-                        started: 'Sep 15, 2020',
-                        ended: '',
-                    },
-                    amount: '220.00',
-                    status: 'INACTIVE',
-                },
-            ];
+        props: ['tableData'],
+        emits: ['action'],
+        setup(props, { emit }) {
+            const handleCommand = (command) => {
+                emit('action', command);
+            };
 
             return {
-                tableData,
+                handleCommand,
+                formatDate,
+                getProductName,
             };
         },
         components: {
