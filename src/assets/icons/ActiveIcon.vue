ced273708664b77167f0b2dcf41ad96368db2116
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-07-07 12:15:35 +0700

    add icon

--- src/assets/icons/ActiveIcon.vue
+++ src/assets/icons/ActiveIcon.vue
@@ -1,7 +1,9 @@
 <template lang="">
-    <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect width="8" height="8" rx="4" transform="matrix(1 0 0 -1 0 8)" fill="#14C8B1" />
-    </svg>
+    <span class="active-icon">
+        <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="8" height="8" rx="4" transform="matrix(1 0 0 -1 0 8)" fill="#14C8B1" />
+        </svg>
+    </span>
 </template>
 <script>
     export default {
---
+++ src/assets/icons/DeleteIcon.vue
@@ -0,0 +1,12 @@
+<template lang="">
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+        <path d="M15 5L5 15" stroke="#EB5757" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M5 5L15 15" stroke="#EB5757" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+</template>
+<script>
+    export default {
+        name: 'DeleteIcon',
+    };
+</script>
+<style lang=""></style>
