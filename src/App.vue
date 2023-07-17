2cc3aa1971659b9954aab0e3e2982756a7520748
Nguyễn Tuấn Việt <83708489+VietNT1810@users.noreply.github.com>
2023-07-17 14:43:51 +0700

    ignore ResizeObserver loop limit exceeded

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,26 @@
 
 <script>
     // import Payment from '@/pages/Payment.vue';
+    import { watchEffect } from 'vue';
     import SearchPage from './pages/SearchPage.vue';
     export default {
-        setup() {},
+        setup() {
+            //ignore ResizeObserver loop limit exceeded
+            watchEffect(() => {
+                window.addEventListener('error', (e) => {
+                    if (e.message === 'ResizeObserver loop limit exceeded') {
+                        const resizeObserverErrDiv = document.getElementById('webpack-dev-server-client-overlay-div');
+                        const resizeObserverErr = document.getElementById('webpack-dev-server-client-overlay');
+                        if (resizeObserverErr) {
+                            resizeObserverErr.setAttribute('style', 'display: none');
+                        }
+                        if (resizeObserverErrDiv) {
+                            resizeObserverErrDiv.setAttribute('style', 'display: none');
+                        }
+                    }
+                });
+            });
+        },
         components: {
             SearchPage,
         },
